dd585beda84ac3a87016e4180d1ca29036213382
qoralsrl03 <qoralsrl03@naver.com>
2023-06-16 15:10:18 +0900

    2023-06-16 파이썬 수업[주식정보 수집]

--- week2/day1/get_stock.py
+++ week2/day1/get_stock.py
@@ -2,6 +2,16 @@ import json
 import requests
 import cx_Oracle
 
+#DB에 다음 테이블 만들어주기
+# CREATE TABLE tb_stock(
+#     itemCode varchar2(100)
+#     ,stockName varchar2(100)
+#     ,closePrice varchar2(100)
+#     ,update_dt DATE DEFAULT SYSDATE
+# );
+
+sql = """INSERT INTO tb_stock(itemCode, stockName, closePrice)
+            values(:1, :2, :3)"""
 def fn_stock_get():
     conn = cx_Oracle.connect("java", "oracle", "localhost:1521/xe")
     for i in range(2):
@@ -14,4 +24,11 @@ def fn_stock_get():
             if len(data['stocks']) == 0:
                 break
             for idx, val in enumerate(data['stocks']):
+                cur = conn.cursor()
+                cur.execute(sql, [val['itemCode'],val['stockName'],val['closePrice']])
+                conn.commit()
                 print(val['itemCode'], val['stockName'], val['closePrice'])
+
+    conn.close()
+if __name__ == '__main__':
+    fn_stock_get()
