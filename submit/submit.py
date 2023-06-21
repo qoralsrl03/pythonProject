ee132347dfb2bd1a2ab0ef6254d5ee5c97b08655
qoralsrl03 <qoralsrl03@naver.com>
2023-06-21 12:28:20 +0900

    2023-06-21 파이썬 발표 과제 중간저장

--- submit/submit.py
+++ submit/submit.py
@@ -1,21 +1,55 @@
+import re
 import tkinter as tk
 import tkinter.font as tkfont
 import tkinter.messagebox as messagebox
+import chromedriver_autoinstaller
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
+from bs4 import BeautifulSoup
+from PIL import Image
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def create_custom_messagebox(window, title, message, font_size):
-    result = messagebox.askokcancel(title, message, parent=window)
+chromedriver_autoinstaller.install()
+UA = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+options = Options()
+options.add_argument(f'--user-agent={UA}')
+
+def create_custom_messagebox(window, text, categories):
+    text = text
+    categories = categories
+    category_str = ', '.join(categories)
+    result = messagebox.askokcancel('입력 재확인', '검색어: ' + text + '\n선택 카테고리: ' + category_str, parent=window)
     if result:
         # [확인] 버튼을 클릭한 경우 처리할 작업을 수행합니다.
-        print("확인 버튼을 클릭했습니다.")
+        driver = webdriver.Chrome(options=options)
+        driver.implicitly_wait(3)  # 대기
+        if not categories:
+            url = f'https://map.naver.com/v5/search/{text}'
+        else:
+            url = f'https://map.naver.com/v5/search/{text}%20{category_str}'
+        driver.get(url)
+        time.sleep(3)
+        divs = driver.find_elements(By.CLASS_NAME, 'face_marker')
+        time.sleep(3)
+        for div in divs:
+            name = div.find_element(By.CLASS_NAME, 'name').text
+            print(name)
+        driver.quit()
     else:
         # [취소] 버튼을 클릭한 경우 처리할 작업을 수행합니다.
-        print("취소 버튼을 클릭했습니다.")
+        messagebox.showinfo("알림", '검색을 취소합니다')
 
 def btn_click():
     text = txt.get()
     selected_indices = listbox.curselection()
     selected_categories = [listbox.get(idx) for idx in selected_indices]
-    create_custom_messagebox(window, 'Go Search', f'입력된 텍스트: {text}\n선택된 카테고리: {selected_categories}', 30)
+    create_custom_messagebox(window, text, selected_categories)
+
+
 
 window = tk.Tk()
 window.geometry("900x700+100+100")
